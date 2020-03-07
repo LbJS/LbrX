865856264cf88a0eaf019771f82fc590c56693bd
unlimited.2033@gmail.com <unlimited.2033@gmail.com>
2020-03-07 18:32:01 +0200

    is class spec

---
+++ tests/helper-functions/is-class.spec.ts
@@ -0,0 +1,12 @@
+import { isClass } from '../../lbrx/src/helpers'
+
+describe('Helper function: isClass', () => {
+	it('Should be constructable', () => {
+		class Person { }
+		const person = new Person()
+		expect(isClass(person)).toBeTruthy()
+	})
+	it('Should not be constructable', () => {
+		expect(isClass({})).toBeFalsy()
+	})
+})
