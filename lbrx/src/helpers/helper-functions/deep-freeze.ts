e62132efb7bd5f4fbe080925dba62d862a075e5d
Leon Bernstein <unlimited.2033@gmail.com>
2020-03-09 14:48:38 +0200

    added date to deep freeze

--- lbrx/src/helpers/helper-functions/deep-freeze.ts
+++ lbrx/src/helpers/helper-functions/deep-freeze.ts
@@ -1,7 +1,25 @@
+function dateThrow(this: Date): never {
+	throw new Error(`This date: "${this.toLocaleString()}" is read only.`)
+}
+
 export function deepFreeze<T extends {}>(object: T): Readonly<T> {
 	for (const name of Object.getOwnPropertyNames(object)) {
 		const value = object[name]
-		if (value && typeof value == 'object') {
+		if (value instanceof Date) {
+			value.setTime = dateThrow
+			value.setFullYear = dateThrow
+			value.setMonth = dateThrow
+			value.setDate = dateThrow
+			value.setHours = dateThrow
+			value.setMinutes = dateThrow
+			value.setMilliseconds = dateThrow
+			value.setUTCFullYear = dateThrow
+			value.setUTCMonth = dateThrow
+			value.setUTCDate = dateThrow
+			value.setUTCHours = dateThrow
+			value.setUTCMinutes = dateThrow
+			value.setUTCMilliseconds = dateThrow
+		} else if (value && typeof value == 'object') {
 			object[name] = deepFreeze(value)
 		}
 	}
--- tests/helper-functions/deep-freeze.spec.ts
+++ tests/helper-functions/deep-freeze.spec.ts
@@ -14,6 +14,17 @@ describe('Helper Function - deepFreeze():', () => {
 		public emails: string[] = [
 			'test@email.com'
 		]
+		public address = {
+			city: 'Hell of a City'
+		}
+		public deepNestedObject = {
+			a: {
+				b: {
+					c: 'nested value'
+				}
+			}
+		}
+		public birthday = new Date('1986-10-12T00:00:00')
 	}
 	let person: Person
 
@@ -22,27 +33,45 @@ describe('Helper Function - deepFreeze():', () => {
 		deepFreeze(person)
 	})
 
-	it(`Object's firstName property should throw on modification`, () => {
+	it(`Person's firstName property should throw on modification`, () => {
 		expect(() => {
 			person.firstName = 'Something Else'
 		}).toThrow()
 	})
 
-	it(`Object's lastName property should throw on modification`, () => {
+	it(`Person's lastName property should throw on modification`, () => {
 		expect(() => {
 			person.lastName = 'Something Else'
 		}).toThrow()
 	})
 
-	it(`Object's emails list should throw throw on adding a new item`, () => {
+	it(`Person's emails list should throw throw on adding a new item`, () => {
 		expect(() => {
 			person.emails.push('newEmail@email.com')
 		}).toThrow()
 	})
 
-	it(`Object's emails list should throw on item's modification`, () => {
+	it(`Person's emails list should throw on item's modification`, () => {
 		expect(() => {
 			person.emails[0] = 'newEmail@email.com'
+		}).toThrow()
+	})
+
+	it(`Person's address should throw on modification`, () => {
+		expect(() => {
+			person.address.city = 'some other city'
+		}).toThrow()
+	})
+
+	it(`Person's deep nested value should throw on modification`, () => {
+		expect(() => {
+			person.deepNestedObject.a.b.c = 'some other value'
+		}).toThrow()
+	})
+
+	it(`Person's birthday should throw on modification`, () => {
+		expect(() => {
+			person.birthday.setFullYear(1987)
 		}).toThrow()
 	})
 })
