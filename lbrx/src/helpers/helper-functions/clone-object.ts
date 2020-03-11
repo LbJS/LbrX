520b9b565fb5d37168106641752242422a99c2df
Leon Bernstein <unlimited.2033@gmail.com>
2020-03-11 22:27:42 +0200

    clone object func

---
+++ lbrx/src/helpers/helper-functions/clone-object.ts
@@ -0,0 +1,24 @@
+import { isDate } from "./is-date"
+import { isArray } from "./is-array"
+import { objectAssign } from "../short-hand-functions/object-assign"
+import { isObject } from "./is-object"
+import { isClass } from "./is-class"
+import { objectKeys } from "../short-hand-functions/object-keys"
+
+export function cloneObject<T = {}>(obj: T): T {
+	if (isDate(obj)) return new Date(obj) as any as T
+	let copy: any = null
+	if (isArray(obj)) {
+		copy = objectAssign([], obj)
+		// tslint:disable-next-line: prefer-for-of
+		for (let i = 0; i < copy.length; i++) {
+			if (isObject(copy[i])) copy[i] = cloneObject(copy[i])
+		}
+	} else if (isObject(obj)) {
+		copy = objectAssign(isClass(obj) ? new (obj as any).constructor() : {}, obj as {})
+		for (let i = 0, keys = objectKeys(copy); i < keys.length; i++) {
+			if (isObject(copy[keys[i]])) copy[keys[i]] = cloneObject(copy[keys[i]])
+		}
+	}
+	return copy ?? obj
+}
