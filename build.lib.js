1252b564a115af36a7dc1d1af039f006a002ca45
Leon Bernstein <unlimited.2033@gmail.com>
2020-03-13 04:07:34 +0200

    unnecessary package files fix

---
+++ build.lib.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+
+// copy README.md
+fs.createReadStream('README.md').pipe(fs.createWriteStream('lib/README.md'))
+
+// copy package.json and remove unnecessary properties
+const rawJsonData = fs.readFileSync('package.json')
+const packageJsonObj = JSON.parse(rawJsonData)
+const unnecessaryPropertiesList = [
+	'scripts',
+	'devDependencies',
+	'jest',
+]
+unnecessaryPropertiesList.forEach(key => {
+	delete packageJsonObj[key]
+})
+packageJsonObj.main = 'index.js'
+fs.writeFileSync('lib/package.json', JSON.stringify(packageJsonObj, null, 2), 'utf-8')
