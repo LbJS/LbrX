94541b2547dfae2bdf2e552498fb59c54f07b0c3
Leon Bernstein <unlimited.2033@gmail.com>
2020-03-19 14:18:36 +0200

    better instanceHandler

--- src/helpers/helper-functions/instance-handler.ts
+++ src/helpers/helper-functions/instance-handler.ts
@@ -1,10 +1,10 @@
 import { objectAssign, objectKeys, isDate, cloneObject, isClass, isObject } from 'lbrx/helpers'
 
 export function instanceHandler<T extends object>(instancedObject: T, plainObject: T): T {
-	if (isDate(instancedObject)) return new Date(plainObject as any) as any as T
+	if (isDate(instancedObject)) return new Date(plainObject as Date | string) as any
 	let copy = cloneObject(plainObject)
 	if (isClass(instancedObject)) {
-		copy = objectAssign<T, T>(new (instancedObject as any).constructor(), plainObject)
+		copy = objectAssign(new instancedObject.constructor(), plainObject)
 	}
 	for (let i = 0, keys = objectKeys(plainObject); i < keys.length; i++) {
 		if (isObject(plainObject[keys[i]])) {
