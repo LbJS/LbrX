b0089e56df15786d99c4f0d2fa3b1ab32b9ff9df
Leon Bernstein <unlimited.2033@gmail.com>
2020-03-19 18:15:15 +0200

    LbrXManager comments

--- src/lbrx-manager.ts
+++ src/lbrx-manager.ts
@@ -1,15 +1,30 @@
 import { DevtoolsOptions, initLbrxDevTools } from './dev-tools'
 import { enableProdMode } from './mode'
+import { throwError } from 'lbrx/helpers'
 
+/**
+ * LbrX static builder class.
+ */
 export class LbrXManager {
 
+	/**
+	 * Enabling production mode will improve performance and
+	 * will log errors instead of throwing them.
+	 */
 	static enableProdMode(): LbrXManager {
 		enableProdMode()
 		return LbrXManager
 	}
 
+	/**
+	 * Initializes Redux DevTools.
+	 */
 	static initializeDevTools(devToolsOptions?: Partial<DevtoolsOptions>): LbrXManager {
 		initLbrxDevTools(devToolsOptions)
 		return LbrXManager
+	}
+
+	constructor() {
+		throwError('LbrXManager class is an static class, no instance is allowed.')
 	}
 }
