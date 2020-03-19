984d3043f76320a4cf21251cd2fbb5aecb71c84b
Leon Bernstein <unlimited.2033@gmail.com>
2020-03-19 14:43:17 +0200

    better cloneObject

--- src/helpers/helper-functions/clone-object.ts
+++ src/helpers/helper-functions/clone-object.ts
@@ -1,18 +1,23 @@
-import { isDate, isArray, objectAssign, isObject, isClass, objectKeys } from 'lbrx/helpers'
+import { isArray, objectAssign, isObject, isClass, objectKeys } from 'lbrx/helpers'
 
 export function cloneObject<T extends object>(obj: T): T {
-	if (isDate(obj)) return new Date(obj) as any
-	let copy: T | [] | null = null
+	let copy: T | any[] | null = null
 	if (isArray(obj)) {
-		copy = objectAssign([], obj)
-		// tslint:disable-next-line: prefer-for-of
+		copy = [...obj]
 		for (let i = 0; i < copy.length; i++) {
 			if (isObject(copy[i])) copy[i] = cloneObject(copy[i])
 		}
 	} else if (isObject(obj)) {
-		copy = objectAssign(isClass(obj) ? new obj.constructor() as T : {} as T, obj)
+		if (isClass(obj)) {
+			copy = obj.constructor.length ?
+				new obj.constructor(obj) as T :
+				objectAssign(new obj.constructor() as T, obj)
+		} else {
+			copy = { ...obj }
+		}
 		for (let i = 0, keys = objectKeys(copy); i < keys.length; i++) {
-			if (isObject(copy[keys[i]])) copy[keys[i]] = cloneObject(copy[keys[i]])
+			const key = keys[i]
+			if (isObject(copy[key])) copy[key] = cloneObject(copy[key])
 		}
 	}
 	return (copy ?? obj) as T
