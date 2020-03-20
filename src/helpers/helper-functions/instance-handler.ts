3c9545e5097a9c160165f4365b581deda18a1840
Leon Bernstein <unlimited.2033@gmail.com>
2020-03-20 12:59:34 +0200

    better instanceHandler

--- src/helpers/helper-functions/instance-handler.ts
+++ src/helpers/helper-functions/instance-handler.ts
@@ -1,24 +1,16 @@
-import { objectAssign, objectKeys, cloneObject, isClass, isObject, isNullish } from 'lbrx/helpers'
+import { objectAssign, objectKeys, isClass, isObject, isNullish } from 'lbrx/helpers'
 
 export function instanceHandler<T extends object>(instancedObject: T, plainObject: T): T {
-	let copy = cloneObject(plainObject)
-	if (isClass(instancedObject) && !isClass(copy)) {
-		copy = instancedObject.constructor.length ?
-			new instancedObject.constructor(plainObject) :
+	if (isNullish(plainObject)) return plainObject
+	if (isClass(instancedObject) && !isClass(plainObject)) {
+		plainObject = instancedObject.constructor.length ?
+			objectAssign(new instancedObject.constructor(plainObject), plainObject) :
 			objectAssign(new instancedObject.constructor(), plainObject)
 	}
-	for (let i = 0, keys = objectKeys(plainObject); i < keys.length; i++) {
-		const key = keys[i]
-		const instancedObj = instancedObject[key]
-		const plainObj = plainObject[key]
-		if (isObject(plainObj)) {
-			copy[key] = instanceHandler(instancedObj, plainObj)
-		} else if (!isNullish(plainObj) &&
-			isClass(instancedObj) &&
-			instancedObj.constructor.length
-		) {
-			copy[key] = new instancedObj.constructor(plainObj)
+	for (let i = 0, keys = objectKeys(instancedObject); i < keys.length; i++) {
+		if (isObject(instancedObject[keys[i]])) {
+			plainObject[keys[i]] = instanceHandler(instancedObject[keys[i]], plainObject[keys[i]])
 		}
 	}
-	return copy
+	return plainObject
 }
