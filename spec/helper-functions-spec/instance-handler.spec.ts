d40b6086dbc4a2780a02234e54ada733123833a0
Leon Bernstein <unlimited.2033@gmail.com>
2020-03-22 23:32:14 +0200

    better instance handler spec

--- spec/helper-functions-spec/instance-handler.spec.ts
+++ spec/helper-functions-spec/instance-handler.spec.ts
@@ -1,32 +1,27 @@
-import { instanceHandler, isDate, simpleCloneObject } from 'lbrx/helpers'
+import { instanceHandler } from 'lbrx/helpers'
 import { Person, Address } from 'test-subjects'
 
 describe('Helper Function - instanceHandler():', () => {
 
 	it('should create instance for plain object.', () => {
-		const person = new Person({
-			firstName: 'leon'
-		})
-		const personCopy = instanceHandler(person, simpleCloneObject(person))
-		expect(personCopy instanceof Person).toBeTruthy()
+		const person = new Person({})
+		const personCopy = instanceHandler(person, { firstName: 'Someone' } as Person)
+		expect(personCopy).toBeInstanceOf(Person)
 	})
 
 	it('should create instance for nested plain object.', () => {
 		const person = new Person({
-			firstName: 'leon',
-			address: new Address({
-				city: 'super city'
-			})
+			address: {}
 		})
-		const personCopy = instanceHandler(person, simpleCloneObject(person))
-		expect(personCopy.address instanceof Address).toBeTruthy()
+		const personCopy = instanceHandler(person, { address: { city: 'some city' } } as Person)
+		expect(personCopy.address).toBeInstanceOf(Address)
 	})
 
 	it('should create instance for date.', () => {
 		const person = new Person({
 			someDate: new Date()
 		})
-		const personCopy = instanceHandler(person, simpleCloneObject(person))
-		expect(isDate(personCopy.someDate)).toBeTruthy()
+		const personCopy = instanceHandler(person, { someDate: new Date().toJSON() as unknown as Date } as Person)
+		expect(personCopy.someDate).toBeInstanceOf(Date)
 	})
 })
