c18f70daf258c4fd62762f09e65ce568c4538c01
Leon Bernstein <unlimited.2033@gmail.com>
2020-03-29 00:28:27 +0300

    global error spec done

--- spec/stores-spec/global-error-store/unlinked-error-reference.spec.ts
+++ spec/stores-spec/global-error-store/unlinked-error-reference.spec.ts
@@ -1,5 +1,5 @@
 import { GlobalErrorStore } from 'lbrx'
-import { createError } from 'test-subjects'
+import { createError, createCustomError, CustomError } from 'test-subjects'
 
 describe('Global Error Store, Error Object:', () => {
 
@@ -14,8 +14,48 @@ describe('Global Error Store, Error Object:', () => {
 		jest.resetModules()
 	})
 
-	it('should be the exact same object.', () => {
+	it('should be the exact same error object.', () => {
 		globalErrorStore.setError(createError())
 		expect(globalErrorStore.getError()).toMatchObject(createError())
+	})
+
+	it('should have different error object reference.', () => {
+		const error = createError()
+		globalErrorStore.setError(error)
+		expect(globalErrorStore.getError()).not.toBe(error)
+	})
+
+	it("should not be effected by error object's change after set.", () => {
+		const errMsg = 'New Error Msg'
+		const error = createError()
+		globalErrorStore.setError(error)
+		error.message = errMsg
+		expect(globalErrorStore.getError()?.message).not.toBe(errMsg)
+	})
+
+	it('should be the exact same nested custom error object.', () => {
+		globalErrorStore.setError(createCustomError())
+		expect(globalErrorStore.getError()).toMatchObject(createCustomError())
+	})
+
+	it('should have different nested custom error object reference.', () => {
+		const error = createCustomError()
+		globalErrorStore.setError(error)
+		expect(error.innerError?.innerError).toBeTruthy()
+		expect((globalErrorStore.getError() as CustomError).innerError?.innerError).toBeTruthy()
+		expect((globalErrorStore.getError() as CustomError).innerError?.innerError).not.toBe(error.innerError?.innerError)
+	})
+
+	it("should not be effected by custom error object's change after set.", () => {
+		const errMsg = 'New Error Msg'
+		const error = createCustomError()
+		globalErrorStore.setError(error)
+		if (error.innerError?.innerError) {
+			error.innerError.innerError.message = errMsg
+		} else {
+			fail('Invalid test subject')
+		}
+		expect((globalErrorStore.getError() as CustomError).innerError?.innerError).toBeTruthy()
+		expect((globalErrorStore.getError() as CustomError).innerError?.innerError).not.toBe(errMsg)
 	})
 })
--- spec/test-subjects/error.ts
+++ spec/test-subjects/error.ts
@@ -1,4 +1,27 @@
+import { isString, isError } from 'lbrx/helpers'
+
+// tslint:disable: unified-signatures
 
 export function createError(): Error {
 	return new Error('Some error text')
+}
+
+export class CustomError extends Error {
+
+	public innerError: CustomError | null = null
+
+	constructor()
+	constructor(errMsg: string)
+	constructor(error: CustomError)
+	constructor(error: CustomError, errMsg: string)
+	constructor(errMsgOrError?: string | CustomError, errMsg?: string) {
+		super(isString(errMsg) ? errMsg : isString(errMsgOrError) ? errMsgOrError : undefined)
+		if (isError(errMsgOrError)) this.innerError = errMsgOrError
+	}
+}
+
+export function createCustomError(): CustomError {
+	const err1 = new CustomError('First Error')
+	const err2 = new CustomError(err1, 'Second Error')
+	return new CustomError(err2)
 }
