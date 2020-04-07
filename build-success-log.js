068a871d6c4f8db36bc6a00489d66f950ea0e720
Leon Bernstein <unlimited.2033@gmail.com>
2020-04-07 03:20:18 +0300

    lib imports validation done

---
+++ build-success-log.js
@@ -0,0 +1,3 @@
+console.log()
+console.log("\x1b[32m", 'LbrX build process is finished.', "\x1b[0m")
+console.log()
--- post-build-imports-test.js
+++ post-build-imports-test.js
@@ -20,18 +20,28 @@ const BABEL_CONFIG = {
 }
 
 const LIBRARY_FOLDER = './lib'
+const JS_FILES_REGEX = /^(?!.*\.map\.js).*\.js$/
+const DECLARATION_FILES_REGEX = /^.*\.d.ts$/
+const VALID_DECLARATION_FILES_PREFIXES = [
+	'./',
+	'lbrx',
+	'rxjs',
+]
 
 main()
 
 async function main() {
 	registerBabel(BABEL_CONFIG)
-	const jsModules = getFilesFromDir(LIBRARY_FOLDER, /^(?!.*\.map\.js).*\.js$/)
+	const jsModules = getFilesFromDir(LIBRARY_FOLDER, JS_FILES_REGEX)
+	const declarationFiles = getFilesFromDir(LIBRARY_FOLDER, DECLARATION_FILES_REGEX)
 	try {
-		await validateAllModules(jsModules)
-		logSuccess()
+		await validateAllJsModules(jsModules)
+		validateDeclarationFiles(declarationFiles)
 	} catch (e) {
 		logError(e)
+		return
 	}
+	logSuccess()
 }
 
 /**
@@ -56,31 +66,53 @@ function getFilesFromDir(dir, regExp) {
  * @param {string[]} jsModules
  * @returns {Promise<void>}
  */
-async function validateAllModules(jsModules) {
+async function validateAllJsModules(jsModules) {
 	return new Promise(async (resolve, reject) => {
-		let errorMsg = null
 		let error = null
 		await Promise.all(jsModules.map(async module => {
 			await import(module).catch(e => {
-				errorMsg = `Can't resolve module name: ${module}`
-				error = e
+				error = {
+					innerError: e,
+					message: `Can't resolve module name: ${module}`,
+				}
 				return
 			})
 		}))
-		error ? reject({ errorMsg, error }) : resolve()
+		error ? reject(error) : resolve()
 	})
 }
 
 /**
- * @param {{ errorMsg, error }} e
+ * @param {string[]} declarationFiles
+ */
+function validateDeclarationFiles(declarationFiles) {
+	declarationFiles.forEach(file => {
+		const fileContent = fs.readFileSync(file, { encoding: 'utf8' })
+		const fileLines = fileContent.split('\n')
+		fileLines.forEach(line => {
+			if (!line.startsWith('export *') && !line.startsWith('import')) return
+			let moduleName = line.split('from ')[1]
+			moduleName = moduleName.substring(1, moduleName.length - 3)
+			const isFileValid = VALID_DECLARATION_FILES_PREFIXES.some(prefix => moduleName.startsWith(prefix))
+			if (!isFileValid) {
+				throw new Error(`Module: "${moduleName}" in file: "${file}" is invalid!`)
+			}
+		})
+	})
+}
+
+/**
+ * @param {{ error, message }} e
  */
 function logError(e) {
 	console.log()
 	console.error("\x1b[31m", 'ERROR!!!')
-	console.error("\x1b[31m", e.errorMsg, "\x1b[0m")
+	console.error("\x1b[31m", e.message, "\x1b[0m")
 	console.log()
-	console.error(e.error)
-	console.log()
+	if (e.innerError) {
+		console.error(e.innerError)
+		console.log()
+	}
 }
 
 function logSuccess() {
