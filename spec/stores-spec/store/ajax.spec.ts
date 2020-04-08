6f0705774684f474c684f3ea00dd1fd032c7a0b6
Leon Bernstein <unlimited.2033@gmail.com>
2020-04-08 22:14:58 +0300

    ajax spec

---
+++ spec/stores-spec/store/ajax.spec.ts
@@ -0,0 +1,31 @@
+import { from } from 'rxjs'
+import { Store } from 'lbrx'
+import { Todo } from 'test-subjects'
+import fetch from 'node-fetch'
+
+describe('Store ajax call:', () => {
+
+	const getData = (): Promise<Todo> => fetch('https://jsonplaceholder.typicode.com/todo1.json').then(r => r.json())
+	let expectedTodo: Todo
+	let store: Store<Todo>
+
+	beforeEach(async () => {
+		expectedTodo = await getData()
+		const providerModule = await import('provider.module')
+		store = providerModule.StoresFactory.createStore<Todo>(null)
+	})
+
+	afterEach(() => {
+		jest.resetModules()
+	})
+
+	it('should get todo item from promise ajax call.', async () => {
+		await store.initializeAsync(getData())
+		expect(store.value).toStrictEqual(expectedTodo)
+	})
+
+	it('should get todo item from observable ajax call.', async () => {
+		await store.initializeAsync(from(getData()))
+		expect(store.value).toStrictEqual(expectedTodo)
+	})
+})
