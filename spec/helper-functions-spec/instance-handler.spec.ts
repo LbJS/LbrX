ed1764ff40536e199e49a7d916a4ec1e8236b0df
Leon Bernstein <unlimited.2033@gmail.com>
2020-04-12 01:39:50 +0300

    better instanced handler spec

--- spec/helper-functions-spec/instance-handler.spec.ts
+++ spec/helper-functions-spec/instance-handler.spec.ts
@@ -1,41 +1,28 @@
+import { assertNotNullable } from 'helpers'
 import { instanceHandler } from 'lbrx/helpers'
-import { Address, Person } from 'test-subjects'
+import { DeepNestedTestSubjectA, InnerTestSubjectA, TestSubjectA, TestSubjectsFactory } from 'test-subjects'
 
-describe('Helper Function - instanceHandler():', () => { // TODO: Refactor
+describe('Helper Function - instanceHandler():', () => {
 
-  it('should create instance for plain object.', () => {
-    const person = new Person({})
-    const personCopy = instanceHandler(person, { firstName: 'Someone' } as Person)
-    expect(personCopy).toBeInstanceOf(Person)
+  let instancedTestSubject: TestSubjectA
+  let plainTestSubject: TestSubjectA
+
+  beforeEach(() => {
+    instancedTestSubject = TestSubjectsFactory.createTestSubjectA_configA()
+    plainTestSubject = TestSubjectsFactory.createTestSubjectA_configA_plain()
   })
 
-  it('should create instance for nested plain object.', () => {
-    const person = new Person({
-      address: {}
-    })
-    const personCopy = instanceHandler(person, { address: { city: 'some city' } } as Person)
-    expect(personCopy.address).toBeInstanceOf(Address)
-  })
-
-  it('should create instance for date.', () => {
-    const person = new Person({
-      someDate: new Date(2020, 0)
-    })
-    const personCopy = instanceHandler(person, { someDate: new Date().toJSON() as unknown as Date } as Person)
-    expect(personCopy.someDate).toBeInstanceOf(Date)
-  })
-
-  it('should create instances for all objects in array.', () => {
-    const person = new Person({
-      someDate: new Date(2020, 0)
-    })
-    const persons = [person]
-    const persons2 = [person, person]
-    const plainPersons = JSON.parse(JSON.stringify(persons2))
-    const personsCopy = instanceHandler(persons, plainPersons)
-    expect(personsCopy[0]).toBeInstanceOf(Person)
-    expect(personsCopy[0].someDate).toBeInstanceOf(Date)
-    expect(personsCopy[1]).toBeInstanceOf(Person)
-    expect(personsCopy[1].someDate).toBeInstanceOf(Date)
+  it('should create an instance for object and all nested objects based on an instanced object.', () => {
+    const result = instanceHandler(instancedTestSubject, plainTestSubject)
+    assertNotNullable(result.innerTestObject)
+    assertNotNullable(result.innerTestObjectGetSet?.deepNestedObj?.objectList)
+    expect(result).toBeInstanceOf(TestSubjectA)
+    expect(result.dateValue).toBeInstanceOf(Date)
+    expect(result.innerTestObject).toBeInstanceOf(InnerTestSubjectA)
+    expect(result.innerTestObject.dateValue).toBeInstanceOf(Date)
+    expect(result.innerTestObjectGetSet).toBeInstanceOf(InnerTestSubjectA)
+    expect(result.innerTestObjectGetSet.dateValue).toBeInstanceOf(Date)
+    expect(result.innerTestObjectGetSet.deepNestedObj).toBeInstanceOf(DeepNestedTestSubjectA)
+    expect(result.innerTestObjectGetSet.deepNestedObj.objectList[0]?.date).toBeInstanceOf(Date)
   })
 })
