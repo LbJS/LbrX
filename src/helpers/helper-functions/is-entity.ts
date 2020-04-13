a6ec678d93bad7a782fd2ebf8965881916671184
Leon Bernstein <unlimited.2033@gmail.com>
2020-04-13 23:00:10 +0300

    is entity spec

--- src/helpers/helper-functions/is-entity.ts
+++ src/helpers/helper-functions/is-entity.ts
@@ -1,6 +1,5 @@
-import { isArray } from './is-array'
-import { isDate } from './is-date'
+import { isArray, isDate, isError } from 'lbrx/helpers'
 
 export function isEntity(value: any): value is object {
-  return value && typeof value == 'object' && !isArray(value) && !isDate(value)
+  return value && typeof value == 'object' && !isArray(value) && !isDate(value) && !isError(value)
 }
