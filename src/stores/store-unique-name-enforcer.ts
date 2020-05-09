36c185880995276767fee6be488355a978c4eb58
Leon Bernstein <unlimited.2033@gmail.com>
2020-05-09 05:17:16 +0300

    typo fix

--- src/stores/store-unique-name-enforcer.ts
+++ src/stores/store-unique-name-enforcer.ts
@@ -16,7 +16,7 @@ const storageKeys: string[] = []
 
 export function validateStorageKey(storageKey: string, storeName: string): void {
   if (storageKeys.includes(storageKey)) {
-    const errorMsg = `This storage key is no unique: "${storageKey}" from store: "${storeName}"!`
+    const errorMsg = `This storage key is not unique: "${storageKey}" from store: "${storeName}"!`
     isDev() ? throwError(errorMsg) : logError(errorMsg)
   } else {
     storageKeys.push(storageKey)
