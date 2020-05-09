6f73b3c773de5e59c57d54d81992f91114ac0196
Leon Bernstein <unlimited.2033@gmail.com>
2020-05-09 03:32:42 +0300

    removed old js

--- npm-build-success.js
+++
@@ -1,3 +0,0 @@
-console.log()
-console.log("\x1b[32m", 'LbrX build was successful.', "\x1b[0m")
-console.log()
--- npm-lint-success.js
+++
@@ -1,3 +0,0 @@
-console.log()
-console.log("\x1b[32m", 'LbrX linting was passed successfully.', "\x1b[0m")
-console.log()
