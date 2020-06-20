df291127f2490a793b27e5e05a7feed39f2faee2
Leon Bernstein <unlimited.2033@gmail.com>
2020-06-20 13:46:04 +0300

    jasmin reporter added

--- spec/helpers/global-after-each.ts
+++ spec/helpers/global-after-each.ts
@@ -1,6 +1,40 @@
 import MockBuilder from './mock-builder'
 
+// tslint:disable-next-line: no-namespace
+declare namespace jasmine {
+  interface CurrentTest {
+    id: string
+    description: string
+    fullName: string
+    failedExpectations: {
+      actual: string
+      error: Error
+      expected: string
+      matcherName: string
+      message: string
+      passed: boolean
+      stack: string
+    }[]
+    passedExpectations: unknown[]
+    pendingReason: string
+    testPath: string
+  }
+  let currentTest: CurrentTest
+  let getEnv: () => ({
+    addReporter: (specObj: {
+      specStarted: (currentTest: CurrentTest) => {},
+      specDone: (currentTest: CurrentTest) => {}
+    }) => {}
+  })
+}
+
+jasmine.getEnv().addReporter({
+  specStarted: result => jasmine.currentTest = result,
+  specDone: result => jasmine.currentTest = result,
+})
+
 globalThis.afterEach(() => {
+  // const currentTest = jasmine.currentTest
   jest.resetModules()
   jest.resetAllMocks()
   MockBuilder.deleteAllMocks()
