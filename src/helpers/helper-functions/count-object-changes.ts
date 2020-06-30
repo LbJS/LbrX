270e46831f79a6f1fd10ead0ec8d06e0e7034dd9
Leon Bernstein <unlimited.2033@gmail.com>
2020-06-30 19:15:30 +0300

    refactoring(count object changes spec an method)

--- src/helpers/helper-functions/count-object-changes.ts
+++ src/helpers/helper-functions/count-object-changes.ts
@@ -4,41 +4,39 @@ import { isDate } from './is-date'
 import { isEmpty } from './is-empty'
 import { isFunction } from './is-function'
 import { isObject } from './is-object'
-import { isUndefined } from './is-undefined'
 
-export function countObjectChanges(objA: object | unknown[], objB: object | unknown[]): number {
+export function countObjectChanges(objA: object | unknown[] | unknown, objB: object | unknown[] | unknown): number {
   if (isEmpty(objA) || isEmpty(objB)) return objA === objB ? 0 : 1
-  if (isDate(objA) || isDate(objB)) return isDate(objA) && isDate(objB) && objA.getTime() == objB.getTime() ? 0 : 1
+  if (isDate(objA)) return (isDate(objB) && objA.getTime() == objB.getTime()) ? 0 : 1
+  if (isDate(objB)) return 1
   let changesCount = 0
-  const comparisonHelper = (x: unknown, y: unknown): void => {
-    if (isObject(x)) {
-      if (isObject(y)) {
-        changesCount += countObjectChanges(x, y)
-      } else {
-        changesCount++
-      }
-    } else if (isFunction(x) || isFunction(y)) {
-      if (isFunction(x) != isFunction(y)) changesCount++
-    } else if (x !== y) {
-      changesCount++
-    }
-  }
   if (isArray(objA)) {
     if (isArray(objB)) {
-      objA.forEach((x, i) => {
-        comparisonHelper(x, objB[i])
-      })
-      if (objB.length > objA.length) changesCount += objB.length - objA.length
+      for (let i = 0; i < objA.length && i < objB.length; i++) {
+        changesCount += countObjectChanges(objA[i], objB[i])
+      }
+      changesCount += Math.abs(objB.length - objA.length)
     } else {
       changesCount++
     }
-  } else {
+  } else if (isArray(objB)) {
+    changesCount++
+  } else if (isObject(objA) && isObject(objB)) {
+    const objBKeys = objectKeys(objB)
+    let keyMatches = 0
     objectKeys(objA).forEach(key => {
-      comparisonHelper(objA[key], objB[key])
+      if (objBKeys.includes(key)) {
+        changesCount += countObjectChanges(objA[key], objB[key])
+        keyMatches++
+      } else {
+        changesCount++
+      }
     })
-    objectKeys(objB).forEach(key => {
-      if (isUndefined(objA[key])) changesCount++
-    })
+    changesCount += objBKeys.length - keyMatches
+  } else if (isFunction(objA) && isFunction(objB)) {
+    return changesCount
+  } else if (objA !== objB) {
+    changesCount++
   }
   return changesCount
 }
