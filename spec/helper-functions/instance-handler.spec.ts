d12f4ba4339b2644e4af1fc133ee7807651da4ba
Leon Bernstein <unlimited.2033@gmail.com>
2020-07-03 23:55:06 +0300

    refactor(spec instance hendler)

--- spec/helper-functions/instance-handler.spec.ts
+++ spec/helper-functions/instance-handler.spec.ts
@@ -1,5 +1,5 @@
 import { TestSubjectFactory } from 'helpers/factories'
-import { toPlainObject } from 'helpers/functions'
+import { assertNotNullable, toPlainObject } from 'helpers/functions'
 import { DeepNestedTestSubject, InnerTestSubject, TestSubject } from 'helpers/test-subjects'
 import { instanceHandler } from 'lbrx/helpers'
 import moment, { Moment } from 'moment'
@@ -8,20 +8,19 @@ describe('Helper Function - instanceHandler():', () => {
 
   const createInstancedObjA = () => TestSubjectFactory.createTestSubject_configA()
   const createPlainObjA = () => TestSubjectFactory.createTestSubject_configA_plain()
-  const instancedResultA: TestSubject = instanceHandler(createInstancedObjA(), createPlainObjA())
 
-  it.each`
-    testId | valueName                                                 | value                                                     | expectedInstance
-    ${1}   | ${'instancedResultA'}                                     | ${instancedResultA}                                       | ${TestSubject}
-    ${2}   | ${'instancedResultA.dateValue'}                           | ${instancedResultA.dateValue}                             | ${Date}
-    ${3}   | ${'instancedResultA.innerTestObject'}                     | ${instancedResultA.innerTestObject}                       | ${InnerTestSubject}
-    ${4}   | ${'instancedResultA.innerTestObject.dateValue'}           | ${instancedResultA.innerTestObject!.dateValue}            | ${Date}
-    ${5}   | ${'instancedResultA.innerTestObjectGetSet'}               | ${instancedResultA.innerTestObjectGetSet}                 | ${InnerTestSubject}
-    ${6}   | ${'instancedResultA.innerTestObjectGetSet.dateValue'}     | ${instancedResultA.innerTestObjectGetSet!.dateValue}      | ${Date}
-    ${7}   | ${'instancedResultA.innerTestObjectGetSet.deepNestedObj'} | ${instancedResultA.innerTestObjectGetSet!.deepNestedObj}  | ${DeepNestedTestSubject}
-    ${8}   | ${'" .objectList[0]?.date'}         | ${instancedResultA.innerTestObjectGetSet!.deepNestedObj!.objectList![0]!.date}  | ${Date}
-  `('should create an instance of $expectedInstance for $valueName. (testId: $testId)', ({ value, expectedInstance }) => {
-    expect(value).toBeInstanceOf(expectedInstance)
+  it('should create an instance for the root object and all nested objects.', () => {
+    const result: TestSubject = instanceHandler(createInstancedObjA(), createPlainObjA())
+    assertNotNullable(result.innerTestObject)
+    assertNotNullable(result.innerTestObjectGetSet?.deepNestedObj?.objectList)
+    expect(result).toBeInstanceOf(TestSubject)
+    expect(result.dateValue).toBeInstanceOf(Date)
+    expect(result.innerTestObject).toBeInstanceOf(InnerTestSubject)
+    expect(result.innerTestObject.dateValue).toBeInstanceOf(Date)
+    expect(result.innerTestObjectGetSet).toBeInstanceOf(InnerTestSubject)
+    expect(result.innerTestObjectGetSet.dateValue).toBeInstanceOf(Date)
+    expect(result.innerTestObjectGetSet.deepNestedObj).toBeInstanceOf(DeepNestedTestSubject)
+    expect(result.innerTestObjectGetSet.deepNestedObj.objectList[0]?.date).toBeInstanceOf(Date)
   })
 
   it('should create a moment object if it is moment', () => {
