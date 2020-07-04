d93a79442bc542b05288f11c3bd81d98ee57246f
Leon Bernstein <unlimited.2033@gmail.com>
2020-07-04 20:58:22 +0300

    refactor(spec compare objects)

--- src/helpers/helper-functions/compare-objects.ts
+++ src/helpers/helper-functions/compare-objects.ts
@@ -1,41 +1,31 @@
 import { objectKeys } from '../short-hand-functions'
 import { isArray } from './is-array'
 import { isDate } from './is-date'
-import { isEmpty } from './is-empty'
 import { isFunction } from './is-function'
+import { isMoment } from './is-moment-object'
 import { isObject } from './is-object'
 
-export function compareObjects(objA: object | unknown[], objB: object | unknown[]): boolean {
-  if (isEmpty(objA) || isEmpty(objB)) return objA === objB
-  if (isDate(objA) && (!isDate(objB) || objA.getTime() != objB.getTime())) return false
-  const compareHelper = (x: unknown, y: unknown): boolean => {
-    if (isObject(x)) {
-      if (!isObject(y) || !compareObjects(x, y)) return false
-    } else if (isFunction(x)) {
-      if (!isFunction(y)) return false
-    } else if (x !== y) {
-      return false
+export function compareObjects(objA: object | unknown[] | unknown, objB: object | unknown[] | unknown): boolean {
+  if (isDate(objA) || isDate(objB)) return isDate(objA) && isDate(objB) && objA.getTime() == objB.getTime()
+  if (isMoment(objA) || isMoment(objB)) return isMoment(objA) && isMoment(objB) && objA.valueOf() == objB.valueOf()
+  if (isArray(objA) && isArray(objB)) {
+    if (objA.length != objB.length) return false
+    for (let i = 0; i < objA.length; i++) {
+      if (!compareObjects(objA[i], objB[i])) return false
     }
     return true
   }
-  if (isArray(objA)) {
-    if (isArray(objB) &&
-      objA.length == objB.length
-    ) {
-      for (let i = 0; i < objA.length; i++) {
-        if (!compareHelper(objA[i], objB[i])) return false
-      }
-      return true
+  if (isObject(objA) && isObject(objB) && !isArray(objA) && !isArray(objB)) {
+    for (let i = 0, keys = objectKeys(objA); i < keys.length; i++) {
+      const key = keys[i]
+      if (!objB.hasOwnProperty(key)) return false
+      if (!compareObjects(objA[key], objB[key])) return false
     }
-    return false
+    for (let i = 0, keys = objectKeys(objB); i < keys.length; i++) {
+      if (!objA.hasOwnProperty(keys[i])) return false
+    }
+    return true
   }
-  for (let i = 0, keys = objectKeys(objA); i < keys.length; i++) {
-    const p = keys[i]
-    if (objA.hasOwnProperty(p) != objB.hasOwnProperty(p)) return false
-    if (!compareHelper(objA[p], objB[p])) return false
-  }
-  for (let i = 0, keys = objectKeys(objB); i < keys.length; i++) {
-    if (!objA.hasOwnProperty(keys[i])) return false
-  }
-  return true
+  if (isFunction(objA) && isFunction(objB)) return true
+  return objA === objB
 }
