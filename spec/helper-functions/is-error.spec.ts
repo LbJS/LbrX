e0499102fe99a0e1cae9065eaaaa95b3eea5319f
Leon Bernstein <unlimited.2033@gmail.com>
2020-07-18 22:58:36 +0300

    spec(clone-error + is-error)

---
+++ spec/helper-functions/is-error.spec.ts
@@ -0,0 +1,17 @@
+import { isError } from 'helpers/functions'
+import { ErrorTestSubject } from 'provider'
+
+describe('Helper Function - isError():', () => {
+
+  it('should return true for array.', () => {
+    expect(isError(new Error())).toBeTruthy()
+  })
+
+  it('should return true for custom error.', () => {
+    expect(isError(new ErrorTestSubject())).toBeTruthy()
+  })
+
+  it('should return false for plain object.', () => {
+    expect(isError({})).toBeFalsy()
+  })
+})
