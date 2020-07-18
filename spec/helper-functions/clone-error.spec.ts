2dcb31210d3fd9b917a33054428f16237669ac65
Leon Bernstein <unlimited.2033@gmail.com>
2020-07-18 22:58:36 +0300

    spec(clone-error + is-error)

---
+++ spec/helper-functions/clone-error.spec.ts
@@ -0,0 +1,73 @@
+import { assertNotNullable } from 'helpers/functions'
+import { cloneError } from 'lbrx/helpers'
+import { ErrorTestSubject } from 'provider'
+
+describe('Helper Function - cloneError():', () => {
+
+  it('should clone Error object.', () => {
+    const error = new Error('Some error text')
+    const copy = cloneError(error)
+    expect(copy).toStrictEqual(error)
+    expect(copy.message).toBe(error.message)
+    expect(copy).toBeInstanceOf(Error)
+    expect(copy).not.toBe(error)
+  })
+
+  it('should clone custom Error object.', () => {
+    const error = new ErrorTestSubject('Some error text')
+    const copy = cloneError(error)
+    expect(copy).toStrictEqual(error)
+    expect(copy.message).toBe(error.message)
+    expect(copy).toBeInstanceOf(Error)
+    expect(copy).toBeInstanceOf(ErrorTestSubject)
+    expect(copy).not.toBe(error)
+  })
+
+  it('should clone custom Error object and inner custom errors.', () => {
+    let error = new ErrorTestSubject('Some error text1')
+    error = new ErrorTestSubject(error, 'Some error text2')
+    error = new ErrorTestSubject(error, 'Some error text3')
+    const copy = cloneError(error)
+    expect(copy).toStrictEqual(error)
+    expect(copy.message).toBe(error.message)
+    assertNotNullable(copy.innerError)
+    assertNotNullable(error.innerError)
+    expect(copy.innerError.message).toBe(error.innerError.message)
+    assertNotNullable(copy.innerError.innerError)
+    assertNotNullable(error.innerError.innerError)
+    expect(copy.innerError.innerError.message).toBe(error.innerError.innerError.message)
+    expect(copy).toBeInstanceOf(Error)
+    expect(copy).toBeInstanceOf(ErrorTestSubject)
+    expect(copy.innerError).toBeInstanceOf(Error)
+    expect(copy.innerError).toBeInstanceOf(ErrorTestSubject)
+    expect(copy.innerError.innerError).toBeInstanceOf(Error)
+    expect(copy.innerError.innerError).toBeInstanceOf(ErrorTestSubject)
+    expect(copy).not.toBe(error)
+    expect(copy.innerError).not.toBe(error.innerError)
+    expect(copy.innerError.innerError).not.toBe(error.innerError.innerError)
+  })
+
+  it("should clone object that doesn't inherit from Error.", () => {
+    class LocalError {
+      constructor(public message: string, public innerError: LocalError | null = null) { }
+    }
+    let error = new LocalError('Some error text1')
+    error = new LocalError('Some error text2', error)
+    error = new LocalError('Some error text3', error)
+    const copy = cloneError(error)
+    expect(copy).toStrictEqual(error)
+    expect(copy.message).toBe(error.message)
+    assertNotNullable(copy.innerError)
+    assertNotNullable(error.innerError)
+    expect(copy.innerError.message).toBe(error.innerError.message)
+    assertNotNullable(copy.innerError.innerError)
+    assertNotNullable(error.innerError.innerError)
+    expect(copy.innerError.innerError.message).toBe(error.innerError.innerError.message)
+    expect(copy).toBeInstanceOf(LocalError)
+    expect(copy.innerError).toBeInstanceOf(LocalError)
+    expect(copy.innerError.innerError).toBeInstanceOf(LocalError)
+    expect(copy).not.toBe(error)
+    expect(copy.innerError).not.toBe(error.innerError)
+    expect(copy.innerError.innerError).not.toBe(error.innerError.innerError)
+  })
+})
--- src/helpers/helper-functions/clone-error.ts
+++ src/helpers/helper-functions/clone-error.ts
@@ -1,27 +1,18 @@
+import { Constructable } from '../../types'
 import { getOwnPropertyNames } from '../short-hand-functions'
 import { cloneObject } from './clone-object'
-import { isArray } from './is-array'
 import { isError } from './is-error'
 import { isObject } from './is-object'
 
-export function cloneError<T extends Error>(error: T): T {
-  if (!isError(error)) return error
-  const copy = new (error as any).constructor()
-  for (let i = 0, keys = getOwnPropertyNames(error); i < keys.length; i++) {
-    const key = keys[i]
-    const errProp = error[key]
-    if (isError(errProp)) {
-      copy[key] = cloneError(errProp)
-    } else if (isArray(errProp)) {
-      copy[key] = [...errProp]
-      for (let j = 0; j < copy[key].length; j++) {
-        if (isObject(copy[key][j])) copy[key][j] = cloneObject(copy[key][j])
-      }
-    } else if (isObject(errProp)) {
-      copy[key] = cloneObject(errProp)
-    } else {
-      copy[key] = errProp
-    }
+export function cloneError<T extends Error | object>(error: T): T {
+  let copy: T = error
+  if (isError(error)) {
+    copy = new (error as any as Constructable).constructor()
+    getOwnPropertyNames(error).forEach(key => {
+      copy[key] = cloneError(error[key])
+    })
+  } else if (isObject(error)) {
+    copy = cloneObject(error)
   }
   return copy
 }
