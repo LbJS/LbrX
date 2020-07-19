9e882c456461a643f8d5d83007f25ff08ac848e6
Leon Bernstein <unlimited.2033@gmail.com>
2020-07-19 23:00:46 +0300

    spec(is-date)

---
+++ spec/helper-functions/is-date.spec.ts
@@ -0,0 +1,16 @@
+import { isDate } from 'lbrx/helpers'
+
+describe('Helper Function - isDate():', () => {
+
+  it('should return true for Date object.', () => {
+    expect(isDate(new Date())).toBeTruthy()
+  })
+
+  it('should return false for undefined.', () => {
+    expect(isDate(undefined)).toBeFalsy()
+  })
+
+  it('should return false for null.', () => {
+    expect(isDate(null)).toBeFalsy()
+  })
+})
