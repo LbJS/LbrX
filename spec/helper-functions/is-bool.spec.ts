ec3a9548d0d4fad05a0834b8fcb4acc8d68d9a64
Leon Bernstein <unlimited.2033@gmail.com>
2020-07-19 21:52:04 +0300

    spec(is-bool)

---
+++ spec/helper-functions/is-bool.spec.ts
@@ -0,0 +1,28 @@
+import { isBool } from 'lbrx/helpers'
+
+describe('Helper Function - isBool():', () => {
+
+  it('should return true for true.', () => {
+    expect(isBool(true)).toBeTruthy()
+  })
+
+  it('should return true for false.', () => {
+    expect(isBool(false)).toBeTruthy()
+  })
+
+  it('should return false for undefined.', () => {
+    expect(isBool(undefined)).toBeFalsy()
+  })
+
+  it('should return false for null.', () => {
+    expect(isBool(null)).toBeFalsy()
+  })
+
+  it('should return false for 0.', () => {
+    expect(isBool(0)).toBeFalsy()
+  })
+
+  it('should return false for empty string.', () => {
+    expect(isBool('')).toBeFalsy()
+  })
+})
