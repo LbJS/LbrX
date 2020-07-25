a5b8bfd7520bd6dba788d0f88a2466ecb24208e9
Leon Bernstein <unlimited.2033@gmail.com>
2020-07-25 21:57:22 +0300

    spec(is-function)

---
+++ spec/helper-functions/is-function.spec.ts
@@ -0,0 +1,22 @@
+import { TestSubjectFactory } from 'helpers/factories'
+import { isFunction } from 'lbrx/helpers'
+import moment from 'moment'
+
+describe('Helper Function - isFunction():', () => {
+
+  it('should return true for function.', () => {
+    expect(isFunction(() => { })).toBeTruthy()
+  })
+
+  it('should return false for plain object.', () => {
+    expect(isFunction({})).toBeFalsy()
+  })
+
+  it('should return false for class.', () => {
+    expect(isFunction(TestSubjectFactory.createTestSubject_initial())).toBeFalsy()
+  })
+
+  it('should return false for moment.', () => {
+    expect(isFunction(moment())).toBeFalsy()
+  })
+})
