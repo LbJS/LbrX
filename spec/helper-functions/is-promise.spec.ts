77abf674f02f1b42efba821c950e62fb7c8344da
Leon Bernstein <unlimited.2033@gmail.com>
2020-07-29 21:47:32 +0300

    spec(is-promise)

---
+++ spec/helper-functions/is-promise.spec.ts
@@ -0,0 +1,32 @@
+import { isPromise } from 'lbrx/helpers'
+
+describe('"isPromise" helper function', () => {
+
+  it('should return true for Promise instance.', () => {
+    expect(isPromise(new Promise(() => { }))).toBeTruthy()
+  })
+
+  it('should return true for fulfilled Promise.', () => {
+    expect(isPromise(Promise.resolve())).toBeTruthy()
+  })
+
+  it('should return true for rejected Promise.', () => {
+    expect(isPromise(Promise.reject())).toBeTruthy()
+  })
+
+  it('should return false for Promise type.', () => {
+    expect(isPromise(Promise)).toBeFalsy()
+  })
+
+  it('should return false for plain object.', () => {
+    expect(isPromise({})).toBeFalsy()
+  })
+
+  it('should return false for null.', () => {
+    expect(isPromise(null)).toBeFalsy()
+  })
+
+  it('should return false for undefined.', () => {
+    expect(isPromise(undefined)).toBeFalsy()
+  })
+})
