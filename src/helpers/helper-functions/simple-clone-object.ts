d9c15a30a7e291e933862978e5e79773d616a40e
Leon Bernstein <unlimited.2033@gmail.com>
2020-08-01 22:53:28 +0300

    spec(simple clone object)

--- src/helpers/helper-functions/simple-clone-object.ts
+++ src/helpers/helper-functions/simple-clone-object.ts
@@ -1,5 +1,6 @@
 import { parse, stringify } from '../short-hand-functions'
+import { isObject } from './is-object'
 
 export function simpleCloneObject<T extends object>(obj: T): T {
-  return parse(stringify(obj))
+  return isObject(obj) ? parse(stringify(obj)) : obj
 }
