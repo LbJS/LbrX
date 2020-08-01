ad5a67af7b51fd83fa89acad75a453f20c08b2c2
Leon Bernstein <unlimited.2033@gmail.com>
2020-08-01 23:00:31 +0300

    spec(throw error)

---
+++ spec/helper-functions/throw-error.spec.ts
@@ -0,0 +1,18 @@
+import { throwError } from 'lbrx/helpers'
+
+describe('Helper Function - throwError():', () => {
+
+  it('should throw error if an error is provided.', () => {
+    const error = new Error('Some Error')
+    expect(() => {
+      throwError(error)
+    }).toThrow(error)
+  })
+
+  it('should create an error from string and throw it if a string is provided.', () => {
+    const error = new Error('Some Error')
+    expect(() => {
+      throwError(error.message)
+    }).toThrow(error)
+  })
+})
