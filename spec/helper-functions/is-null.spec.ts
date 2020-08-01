85334d04f17d9300fc888d63c70f65996874c6bb
Leon Bernstein <unlimited.2033@gmail.com>
2020-08-01 23:30:13 +0300

    spec(is null)

---
+++ spec/helper-functions/is-null.spec.ts
@@ -0,0 +1,30 @@
+import { isNull } from 'lbrx/helpers'
+
+describe('Helper Function - isNull():', () => {
+
+  it('should return true for null.', () => {
+    expect(isNull(null)).toBeTruthy()
+  })
+
+  it('should return false for undefined', () => {
+    expect(isNull(undefined)).toBeFalsy()
+  })
+
+  it('should return false for false', () => {
+    expect(isNull(false)).toBeFalsy()
+  })
+
+  it('should return false for 0', () => {
+    expect(isNull(0)).toBeFalsy()
+  })
+
+  it('should return false for empty string', () => {
+    expect(isNull('')).toBeFalsy()
+  })
+
+  it('should return false for undefined variable.', () => {
+    // tslint:disable-next-line: prefer-const
+    let empty: undefined
+    expect(isNull(empty)).toBeFalsy()
+  })
+})
