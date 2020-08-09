4b2c66cac416f8522b30b647462c09fdae5b96b3
Leon Bernstein <unlimited.2033@gmail.com>
2020-08-09 23:03:22 +0300

    spec(is-object-cloned)

--- src/helpers/helper-functions/is-object-cloned.ts
+++ src/helpers/helper-functions/is-object-cloned.ts
@@ -2,9 +2,14 @@ import { compareObjects } from './compare-objects'
 import { isObject } from './is-object'
 
 export function isObjectCloned(objA: {}, objB: {}): boolean {
-  if (!compareObjects(objA, objB)) return false
-  if (!isObject(objA) || !isObject(objB)) return false
-  if (objA === objB || objA.constructor.name != objB.constructor.name) return false
+  if (!compareObjects(objA, objB)
+    || !isObject(objA)
+    || !isObject(objB)
+    || objA === objB
+    || objA.constructor.name != objB.constructor.name
+  ) {
+    return false
+  }
   for (const key in objA) {
     if (isObject(objA[key]) && !isObjectCloned(objA[key], objB[key])) return false
   }
