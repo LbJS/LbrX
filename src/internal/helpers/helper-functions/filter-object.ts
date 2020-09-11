f72af228a553eae7a3ef40aab7a16e42969f7c72
Leon Bernstein <unlimited.2033@gmail.com>
2020-09-11 22:41:25 +0300

    spec(filter objects) refactor(filter objects) spec(assert)

--- src/internal/helpers/helper-functions/filter-object.ts
+++ src/internal/helpers/helper-functions/filter-object.ts
@@ -2,10 +2,12 @@ import { KeyOf } from '../../types'
 import { objectKeys } from '../shortened-functions'
 import { cloneObject } from './clone-object'
 
-export function filterObject<T extends R, R extends object>(obj: T, keys: KeyOf<R>[]): R {
+export function filterObject<T extends R, R extends object>(obj: T, keys: KeyOf<R>[], isRemoveKeys: boolean = true): R {
+  if (!obj) return obj
   const copy = cloneObject(obj)
   objectKeys(copy).forEach(key => {
-    if (!keys.includes(key as KeyOf<R>)) delete copy[key]
+    if (keys.includes(key as KeyOf<R>) && isRemoveKeys) delete copy[key]
+    else if (!keys.includes(key as KeyOf<R>) && !isRemoveKeys) delete copy[key]
   })
   return copy
 }
