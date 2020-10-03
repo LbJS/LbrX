308e172c9f150f0847ae06abc5ccf0c5d19dacf9
Leon Bernstein <unlimited.2033@gmail.com>
2020-10-03 21:16:08 +0300

    spec(stack tracing errors)

--- spec/__test__/module-provider.ts
+++ spec/__test__/module-provider.ts
@@ -1,6 +1,3 @@
-import { LbrXManager } from 'lbrx/core'
-const _LbrXManager = LbrXManager.enableStackTracingErrors()
-export * from 'lbrx/dev-tools'
 export * from 'lbrx/internal/core'
 export * from 'lbrx/internal/dev-tools'
 export * from 'lbrx/internal/helpers'
@@ -9,5 +6,4 @@ export * from 'lbrx/internal/stores/config'
 export * from 'lbrx/internal/types'
 export * from './factories'
 export * from './test-subjects'
-export { _LbrXManager as LbrXManager }
 
