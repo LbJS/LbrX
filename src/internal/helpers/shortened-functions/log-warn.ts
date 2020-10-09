dea45b3e39abf065a8b96b073d901b44142a80ae
Leon Bernstein <unlimited.2033@gmail.com>
2020-10-09 18:01:36 +0300

    refactor(log error and warn)

--- src/internal/helpers/shortened-functions/log-warn.ts
+++ src/internal/helpers/shortened-functions/log-warn.ts
@@ -1,4 +1,4 @@
 
 export function logWarn(message: any, ...optionalParams: any[]): void {
-  optionalParams.length ? console.warn(message, optionalParams) : console.warn(message)
+  console.warn(message, optionalParams)
 }
