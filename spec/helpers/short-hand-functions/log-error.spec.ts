f653d280993b94837a5c1b34e2df392411c9ca39
Leon Bernstein <unlimited.2033@gmail.com>
2020-10-09 18:01:36 +0300

    refactor(log error and warn)

--- spec/helpers/short-hand-functions/log-error.spec.ts
+++ spec/helpers/short-hand-functions/log-error.spec.ts
@@ -13,4 +13,10 @@ describe('Short Hand Function - logError():', () => {
     logError('test', new Error('Test'))
     expect(consoleErrorSpy).toBeCalledTimes(1)
   })
+
+  it('should log warning with optional params.', () => {
+    const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation()
+    logError('test', new Error('Test1'), new Error('Test2'))
+    expect(consoleErrorSpy).toBeCalledTimes(1)
+  })
 })
--- src/internal/helpers/shortened-functions/log-error.ts
+++ src/internal/helpers/shortened-functions/log-error.ts
@@ -1,7 +1,4 @@
 
-export function logError(error: Error): void
-export function logError(message: string): void
-export function logError(message: string, error: Error): void
-export function logError(errorOrMessage: string | Error, error?: Error): void {
-  error ? console.error(errorOrMessage, error) : console.error(errorOrMessage)
+export function logError(message?: any, ...optionalParams: any[]): void {
+  console.error(message, optionalParams)
 }
