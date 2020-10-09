175782689330b9d5a610f0bda5907843e8b384c1
Leon Bernstein <unlimited.2033@gmail.com>
2020-10-09 18:50:23 +0300

    spec(newDate) spec(getOwnPropertyNames)

--- spec/helpers/short-hand-functions/log-error.spec.ts
+++ spec/helpers/short-hand-functions/log-error.spec.ts
@@ -2,21 +2,16 @@ import { logError } from 'lbrx/utils'
 
 describe('Short Hand Function - logError():', () => {
 
-  it('should log warning with optional params.', () => {
+  it('should log error.', () => {
     const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation()
     logError('test')
     expect(consoleErrorSpy).toBeCalledTimes(1)
   })
 
-  it('should log warning with optional params.', () => {
+  it('should log error with optional params.', () => {
     const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation()
     logError('test', new Error('Test'))
-    expect(consoleErrorSpy).toBeCalledTimes(1)
-  })
-
-  it('should log warning with optional params.', () => {
-    const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation()
     logError('test', new Error('Test1'), new Error('Test2'))
-    expect(consoleErrorSpy).toBeCalledTimes(1)
+    expect(consoleErrorSpy).toBeCalledTimes(2)
   })
 })
--- src/internal/helpers/shortened-functions/new-date.ts
+++ src/internal/helpers/shortened-functions/new-date.ts
@@ -1,4 +1,27 @@
+import { isUndefined } from '../helper-functions'
 
-export function newDate(value: string | Date): Date {
-  return new Date(value)
+export function newDate(value: string | number | Date): Date
+export function newDate(): Date
+export function newDate(value: string | number): Date
+export function newDate(
+  year: number,
+  month: number,
+  date?: number | undefined,
+  hours?: number | undefined,
+  minutes?: number | undefined,
+  seconds?: number | undefined,
+  ms?: number | undefined,
+): Date
+export function newDate(
+  valueOrYear?: string | number | Date,
+  month?: number,
+  date?: number | undefined,
+  hours?: number | undefined,
+  minutes?: number | undefined,
+  seconds?: number | undefined,
+  ms?: number | undefined,
+): Date {
+  if (isUndefined(valueOrYear)) return new Date()
+  if (isUndefined(month)) return new Date(valueOrYear)
+  return new Date(valueOrYear as number, month, date, hours, minutes, seconds, ms)
 }
