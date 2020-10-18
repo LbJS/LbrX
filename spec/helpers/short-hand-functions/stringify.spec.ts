04e434fc825c633b04a367620f638252f1b58b8e
Leon Bernstein <unlimited.2033@gmail.com>
2020-10-18 23:31:35 +0300

    spec(stringify)

---
+++ spec/helpers/short-hand-functions/stringify.spec.ts
@@ -0,0 +1,26 @@
+import { stringify } from 'lbrx/internal/helpers'
+
+describe('Short Hand Function - stringify():', () => {
+
+  it('should stringify object.', () => {
+    const obj = { firstName: 'mike', lastName: 'test' }
+    expect(stringify(obj)).toBe(JSON.stringify(obj))
+  })
+
+  it('should stringify null if null is provided', () => {
+    expect(stringify(null)).toBe(JSON.stringify(null))
+  })
+
+  it('should stringify object with a replacer.', () => {
+    const foo = { foundation: 'Mozilla', model: 'box', week: 45, transport: 'car', month: 7 }
+    const replacer = (key: string, value: unknown) => typeof value === 'string' ? undefined : value
+    expect(stringify(foo, replacer)).toBe(JSON.stringify(foo, replacer))
+  })
+
+  it('should stringify object with a space.', () => {
+    const foo = { foundation: 'Mozilla', model: 'box', week: 45, transport: 'car', month: 7 }
+    const replacer = (key: string, value: unknown) => typeof value === 'string' ? undefined : value
+    expect(stringify(foo, replacer, '    ')).toBe(JSON.stringify(foo, replacer, '    '))
+    expect(stringify(foo, null, 4)).toBe(JSON.stringify(foo, null, 4))
+  })
+})
--- src/internal/helpers/shortened-functions/stringify.ts
+++ src/internal/helpers/shortened-functions/stringify.ts
@@ -1,8 +1,18 @@
 
 export function stringify(
   value: any,
-  replacer?: (this: any, key: string, value: any) => any | (number | string)[] | null,
+  replacer?: (this: any, key: string, value: any) => any,
+  space?: string | number
+): string
+export function stringify(
+  value: any,
+  replacer?: (number | string)[] | null,
+  space?: string | number
+): string
+export function stringify(
+  value: any,
+  replacer?: ((this: any, key: string, value: any) => any) | (number | string)[] | null,
   space?: string | number
 ): string {
-  return JSON.stringify(value, replacer, space)
+  return JSON.stringify(value, replacer as any, space)
 }
