efd84c9fcd70f8b8bd2e3ad242e774a08a06c48f
Leon Bernstein <unlimited.2033@gmail.com>
2022-05-05 17:30:19 +0300

    feature(playground datepicker styles) fix(datepicker year picker scroll)

--- playground/src/utils/init-datepicker.ts
+++ playground/src/utils/init-datepicker.ts
@@ -1,8 +1,28 @@
 import { mergeObjects } from 'lbrx/utils'
+import { Datepicker } from 'materialize-css'
+
+function scrollToSelectedYear(yearSelectDropdownEl: HTMLElement): void {
+  yearSelectDropdownEl.style.position = `fixed`
+  yearSelectDropdownEl.style.scrollBehavior = `smooth`
+}
+
+function onDraw(datepicker: Datepicker): void {
+  setTimeout(() => {
+    const modalEl: HTMLElement | undefined = datepicker[`modalEl`]
+    const yearSelectDropdownTriggerEl = modalEl?.querySelector(`.select-wrapper.select-year input.select-dropdown.dropdown-trigger`)
+    const yearSelectDropdownElId = yearSelectDropdownTriggerEl?.getAttribute(`data-target`)
+    if (!yearSelectDropdownElId) return
+    const yearSelectDropdownEl = document.getElementById(yearSelectDropdownElId)
+    if (!yearSelectDropdownEl) return
+    yearSelectDropdownTriggerEl?.addEventListener(`click`, () => scrollToSelectedYear(yearSelectDropdownEl), { once: true })
+  }, 250)
+}
 
 function getDefaultOptions(): Partial<M.DatepickerOptions> {
   return {
     container: document.getElementsByTagName(`body`)[0],
+    yearRange: 50,
+    onDraw: onDraw as unknown as any
   }
 }
 
